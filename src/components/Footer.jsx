b373a45ebc594c64b7742ec3d3d5046db5144ea1
Nicolas Basilico <nbasilico@ba.grupobazar.local>
2023-04-03 12:10:37 -0300

    ultimos cambios

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,8 +6,8 @@ const Footer = () => {
         <div className=""><h1 className="">Redes sociales</h1></div>
         <div className="iconos">
             <a href="https://www.instagram.com/nicobasilico1" target="_blank" rel="noreferrer"><i className="fa-brands fa-instagram"></i></a>
-            <a href="https://www.facebook.com/" target="_blank" rel="noreferrer"><i className="fa-brands fa-facebook"></i></a>
-            <a href="https://www.youtube.com/" target="_blank" rel="noreferrer"><i className="fa-brands fa-youtube"></i></a>
+            <a href="https://www.facebook.com/rockeando73" target="_blank" rel="noreferrer"><i className="fa-brands fa-facebook"></i></a>
+            <a href="https://www.youtube.com/lafederablues" target="_blank" rel="noreferrer"><i className="fa-brands fa-youtube"></i></a>
             </div>
         </div>
     )
