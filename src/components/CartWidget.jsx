30a22efc5dbb443a5429b67d9b91a918c724875c
Nicolas Basilico <nbasilico@ba.grupobazar.local>
2023-04-03 12:52:46 -0300

    ultimos retoques

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -4,7 +4,7 @@ const CartWidget = () => {
     return (
         <button type="button" className="btn white position-relative">
             <img src={"/img/cart2.svg"} alt={"Carrito"} width={32} />
-<span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">+5</span>
+<span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">+2</span>
 </button>
 
     )
